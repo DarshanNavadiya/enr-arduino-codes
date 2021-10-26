b386f50bf34f4fdb264d5c0eb11583ca1e8cc85d
Harin Khakhi <39360852+HarinKhakhi@users.noreply.github.com>
2021-10-26 12:46:04 +0530

    hard coded transeiver

--- transceiver.ino
+++ transceiver.ino
@@ -9,6 +9,9 @@ RF24 radio(9, 8);  // CE, CSN
 //address through which two modules communicate.
 const byte address[6] = "00001";
 
+int counter = 0;
+const int delay_counter = 5000;
+
 void setup()
 {
   radio.begin();
@@ -21,9 +24,22 @@ void setup()
 }
 void loop()
 {
-  //Send message to receiver
-  const char text[] = "Hello Darshan";
-  radio.write(&text, sizeof(text));
+  const char forward[] = "HLHL";
+  const char right[] = "HLLLL";
+  const char left[]= "LLHL";
+  const char back[] = "LLLL";
+
+  if(counter == 0){
+    radio.write(&forward, sizeof(forward));
+  }else if(counter == 1){
+    radio.write(&right, sizeof(right));
+  }else if(counter == 2){
+    radio.write(&left, sizeof(left));
+  }else{
+    radio.write(&back, sizeof(back));
+    counter = -1;
+  }
   
-  delay(1000);
+  counter++;
+  delay(delay_counter);
 }
