a1d367cae6f471c657d1b7f5efa384d09f53b139
Harin Khakhi <39360852+HarinKhakhi@users.noreply.github.com>
2021-10-26 13:57:15 +0530

    update

--- receiver.ino
+++ receiver.ino
@@ -9,6 +9,9 @@ RF24 radio(9, 8);  // CE, CSN
 //address through which two modules communicate.
 const byte address[6] = "00001";
 
+// motor
+const int IN[] = {4, 5, 6, 7};
+
 void setup()
 {
   while (!Serial);
@@ -23,13 +26,20 @@ void setup()
   radio.startListening();
 }
 
+void writeToMotor(char logic, int index){
+  if(logic == 'H')
+  digitalWrite(IN[index], HIGH);
+  else
+  digitalWrite(IN[index], LOW);
+}
 void loop()
 {
   //Read the data if available in buffer
   if (radio.available())
   {
-    char text[32] = {0};
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
+    char command[10] = {0};
+    radio.read(&command, sizeof(command));
+    for(int i=0;i<4;i++)
+      writeToMotor(command[i], i);    
   }
 }
